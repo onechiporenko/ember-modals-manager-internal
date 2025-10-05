6de4940d5cae457c8b4caca3a8d7e96199f1c07e
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2025-10-05 15:23:42 +0300

    use pnpm instead of npm

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -5,7 +5,7 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
-    packageManager: 'npm',
+    packageManager: 'pnpm',
     scenarios: [
       {
         name: 'ember-lts-5.4',
